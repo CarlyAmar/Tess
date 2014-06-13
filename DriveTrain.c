3b213bab1c0f2b3768a9b0100c6d2a70905f79bf
Zachary <zalfakir@yahoo.com>
2014-06-13 09:29:41 -0400

    Added safety to stop servos if something is wrong (will make main loop false and end the robot program) Added turning Some cleanup

--- DriveTrain.c
+++ DriveTrain.c
@@ -4,16 +4,59 @@
 #include "Tess.h"
 
 #define DRIVE_COG 1
-#define DRIVE_SPEED 64
 
 extern volatile int leftSpeed;
 extern volatile int rightSpeed;
+extern volatile enum status robot_status;
 
 unsigned int stack[40 + 25];
 static int driveCog;
 
+void driveForward(int x)
+{
+    if (x < 0)
+    {
+        int s1 = servo_speed(LEFT_SERVO, DRIVE_SPEED);
+        int s2 = servo_speed(RIGHT_SERVO, DRIVE_SPEED);
+        servoCheck(s1,s2,debug("Error: In void driveForward(int %i)",x));
+    }
+    else if (x == 0)
+    {
+        stop();
+    }
+    else
+    {
+        int s1 = servo_angle(LEFT_SERVO, distToAngle(x));
+        int s2 = servo_angle(RIGHT_SERVO, distToAngle(x));
+        servoCheck(s1,s2,debug("Error: in void driveForward(int %i)",x));
+    }
+}
+int stop()
+{
+    int c;
+    {
+        int s1 = servo_speed(LEFT_SERVO, 0);
+        int s2 = servo_speed(RIGHT_SERVO, 0);
+
+        c = servoCheck(s1,s2,"Error: In void stop()\n");
+    }
+    if (c !=0)
+    {
+        return -1;
+    }    
+    else
+    {
+        return 0;
+    }   
+}
+
 void initDrive()
 {
+    debug("Initializing Drivetrain\n", 0);
+    /*
+    servo_motor left;
+    servo_motor right;
+    */
     leftSpeed = 0;
     rightSpeed = 0;
     debug("Start driveCog\n", 0);
@@ -43,18 +86,53 @@ void testDrive()
         //TODO find out why abdrive wont compile and use drive_speed to set the speed to the volatile speed ints
         //pause(250);
 }
-void turn()
+void turn(enum direction side)
 {
-    leftSpeed = -1;
-    rightSpeed = 1;
+    if (side == LEFT)
+    {
+        int s1 = servo_angle(LEFT_SERVO, -1800);//negative 
+        int s2 = servo_angle(RIGHT_SERVO, 1800);
+        servoCheck(s1,s2,"Error: in void turn(enum direction LEFT)\n");
+    }
+    else if (side == RIGHT)
+    {
+        int s1 = servo_angle(LEFT_SERVO, 1800);
+        int s2 = servo_angle(RIGHT_SERVO, -1800);
+        servoCheck(s1,s2,"Error: in void turn(enum direction RIGHT)\n");
+    }
+    else
+    {
+        debug("Error: Cannot Turn, Error unknown\n", 0);
+    }
 }
-void setLeftEncoder()
+int servoCheck(int l, int r, char* x)
 {
-    set_output(LED_1, input(LEFT_ENCODER_PIN)); //This encoder does not work
+    if (l != LEFT_SERVO || r != RIGHT_SERVO)
+    {
+        if (l != LEFT_SERVO)
+        {
+            debug("Left Servo Error Code: %i\n", l);
+        }
+        if (r != RIGHT_SERVO)
+        {
+            debug("Right Servo Error Code: %i\n", r);
+        }
+        debug(x, 0);
+        debug("Now Exiting!\n", 0);
+        robot_status = DISABLED;
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
 }
-void setRightEncoder()
+
+int distToAngle(int x)//TODO check this
 {
-    set_output(LED_2, input(RIGHT_ENCODER_PIN));
+    int i = 360 / WHEEL_CIRCUMFERENCE; //how many degrees for each cm
+    int angle = i*x;
+    return angle;
 }
 void checkDriveTrain()
 {
@@ -65,11 +143,10 @@ void setEncoders()
 {
     //Will set present encoder outputs to the LED's
     #ifdef LEFT_ENCODER
-    setLeftEncoder();
+    set_output(LED_1, input(LEFT_ENCODER_PIN)); //This encoder does not work
     #endif
 
     #ifdef RIGHT_ENCODER
-    setRightEncoder();
+    set_output(LED_2, input(RIGHT_ENCODER_PIN));
     #endif
 }
-
--- DriveTrain.h
+++ DriveTrain.h
@@ -5,6 +5,12 @@
 #include "Ports.h"
 #include "fdserial.h"
 
+struct servo_motor {
+    int speed;
+    int port;
+} left, right;
+
+
 void driveForward(int);
 void driving(int, int);
 void driveBackward(float);
@@ -14,12 +20,11 @@ int getRightTicks();
 float getLeftDistance();
 float getRightDistance();
 void turn(); //default 90 degrees
-void stop();
+int stop();
 void stopLeftSide();
 void stopRightSide();
 void setEncoders(); //enables defined encoders
-void setLeftEncoder(); //enables lefts
-void setRightEncoder(); //enables right
 void checkDriveTrain(); //prints the speeds given
-
+int distToAngle(int); //changes a distance to an angle for the Servo's
+int servoCheck(int, int, char*);
 #endif
