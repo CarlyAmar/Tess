eee8ad166bf2e7cbbf36b954558f29ef3133fb4e
Zachary <zalfakir@yahoo.com>
2014-06-13 09:25:09 -0400

    Added additional settings and ports

--- Ports.h
+++ Ports.h
@@ -2,16 +2,23 @@
 #define PORTS
 
 //Define pins here
-#define LEFT_SERVO 13
+#define IR_1 0
+
+#define ULTRASONIC_PIN 2
+#define BUTTON 10
+
 #define RIGHT_SERVO 12
+#define LEFT_SERVO 13
+
+#define IR_2 13
+
 #define LEFT_ENCODER_PIN 14
 #define RIGHT_ENCODER_PIN 15
-#define IR_1 0
-#define IR_2 13
+
 #define LED_1 26
 #define LED_2 27
-#define BUTTON 10
-#define ULTRASONIC_PIN 2
+
+
 //SD card
 #define DO 22
 #define CLK 23
@@ -25,5 +32,7 @@
 #define FILTER_AMOUNT 30
 #define DISTANCE_SIZE 10
 #define LOG_SIZE 100
+#define DRIVE_SPEED 50
+#define WHEEL_CIRCUMFERENCE 25 //in cm
 static int count_const = 100;
 #endif
